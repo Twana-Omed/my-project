0bfd4e1723faa354ae95a56e39322e06d5a7e654
Twana Omed <112326906+Twana-Omed@users.noreply.github.com>
2023-09-08 19:17:11 +0300

    twana chat project

---
+++ chat project/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const messageInput = document.getElementById("message-input");
+    const sendButton = document.getElementById("send-button");
+    const chatMessages = document.getElementById("chat-messages");
+
+    sendButton.addEventListener("click", function () {
+        const message = messageInput.value.trim();
+        if (message !== "") {
+            appendMessage("You", message);
+            messageInput.value = "";
+        }
+    });
+
+    function appendMessage(sender, message) {
+        const messageElement = document.createElement("div");
+        messageElement.classList.add("message");
+        messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
+        chatMessages.appendChild(messageElement);
+        chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
+});
